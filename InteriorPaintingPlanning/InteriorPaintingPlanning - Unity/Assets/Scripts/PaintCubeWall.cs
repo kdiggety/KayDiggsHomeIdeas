ee0cce0458cfc52cbb2ddaac462b7d2a7147a76b
Ken Lewis <klewisjr@hotmail.com>
2013-09-17 20:21:15 -0700

    learning how to set colors

---
+++ InteriorPaintingPlanning/InteriorPaintingPlanning - Unity/Assets/Scripts/PaintCubeWall.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class PaintCubeWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//texture.FloodFillArea(10,10,Color.red);
+    	// When you're done call Apply to commit your changes.
+    	//texture.Apply();
+	}
+}
